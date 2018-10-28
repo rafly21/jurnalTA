12d47d3fbb97b950cdb57aa8209324fcf9e7549e
rafly21 <44397780+rafly21@users.noreply.github.com>
2018-10-28 22:18:30 +0700

    database

--- jurnal.sql
+++ jurnal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2018 at 12:02 PM
+-- Generation Time: Oct 28, 2018 at 04:17 PM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -43,7 +43,21 @@ INSERT INTO `auth` (`id_user`, `username`, `password`, `permission`) VALUES
 (1, 'm', '6f8f57715090da2632453988d9a1501b', 'manajemen'),
 (5, 'p', '83878c91171338902e0fe0fb97a8c47a', 'pengelola'),
 (10, 'p1', 'ec6ef230f1828039ee794566b9c58adc', 'pengelola'),
-(14, 'pr1', 'e060bb629c10e1b143614cc1e9ccdc67', 'pengelola');
+(14, 'pr1', 'e060bb629c10e1b143614cc1e9ccdc67', 'pengelola'),
+(15, 'gg', '73c18c59a39b18382081ec00bb456d43', 'pengelola'),
+(16, 'hh', '5e36941b3d856737e81516acd45edc50', 'pengelola');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bulan_penerbitan`
+--
+
+CREATE TABLE `bulan_penerbitan` (
+  `id` int(11) NOT NULL,
+  `id_jurnal` int(11) NOT NULL,
+  `bulan_terbit` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -66,9 +80,11 @@ CREATE TABLE `data_pengelola` (
 
 INSERT INTO `data_pengelola` (`id_pengelola`, `id_user`, `nama`, `email`, `no_telp`, `foto`) VALUES
 (1, 1, 'LPPM', 'lppm@undip.ac.id', 81097654, 'a.jpg'),
-(3, 5, 'BONI', 'besudigdo@gmail.com', 87731437, 'b.jpg'),
+(3, 5, 'BONI', 'besudigdo@gmail.com', 87731438, 'b.jpg'),
 (8, 10, 'reza', 'rezafp@gmail.com', 87341267, ''),
-(12, 14, 'pri', 'pri@gmail.com', 8765431, '');
+(12, 14, 'pri', 'pri@gmail.com', 8765431, ''),
+(13, 15, 'Robi', 'robi@gmail.com', 8764532, ''),
+(14, 16, 'amien', 'amien@gmail.com', 877314567, '');
 
 -- --------------------------------------------------------
 
@@ -77,7 +93,7 @@ INSERT INTO `data_pengelola` (`id_pengelola`, `id_user`, `nama`, `email`, `no_te
 --
 
 CREATE TABLE `departemen` (
-  `id_dept` int(4) NOT NULL,
+  `id_dept` int(11) NOT NULL,
   `nama_dept` varchar(50) NOT NULL,
   `id_fak` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -185,7 +201,24 @@ INSERT INTO `departemen` (`id_dept`, `nama_dept`, `id_fak`) VALUES
 (115, 'Doktor Teknik Arsitektur', 13),
 (116, 'Doktor Teknik Sipil', 13),
 (117, 'Doktor Teknik Mesin', 13),
-(118, 'Doktor Teknik Kimia', 13);
+(118, 'Doktor Teknik Kimia', 13),
+(119, 'Program Studi Ilmu Kebidanan Dan Penyakit Kandunga', 6),
+(120, 'Program Studi Ilmu Kedokteran Forensik', 6),
+(121, 'Program Studi Ilmu Kesehatan Anak', 6),
+(122, 'Program Studi Ilmu Patologi Anatomi', 6),
+(123, 'Program Studi Ilmu Patologi Klinik', 6),
+(124, 'Program Studi Ilmu Penyakit Dalam', 6),
+(125, 'Program Studi Ilmu Penyakit Jantung Dan Pembuluh D', 6),
+(126, 'Program Studi Ilmu Penyakit Kulit Dan Kelamin', 6),
+(127, 'Program Studi Ilmu Penyakit Mata', 6),
+(128, 'Program Studi Ilmu Penyakit Syaraf', 6),
+(129, 'Program Studi Ilmu Penyakit Tht', 6),
+(130, 'Program Studi Mikrobiologi Klinik', 6),
+(131, 'Program Studi Psikiatri', 6),
+(132, 'Program Studi Radiologi', 6),
+(133, 'Program Studi Rehabilitasi Medik', 6),
+(134, 'Program Studi Gizi Klinik', 6),
+(135, 'Program Studi Ilmu Bedah', 6);
 
 -- --------------------------------------------------------
 
@@ -224,36 +257,33 @@ INSERT INTO `fakultas` (`id_fak`, `nama_fak`) VALUES
 --
 
 CREATE TABLE `jurnal` (
-  `id_jurnal` int(4) NOT NULL,
-  `no_jurnal` int(4) NOT NULL,
+  `id_jurnal` int(11) NOT NULL,
+  `no_jurnal` int(11) NOT NULL,
   `id_pengelola` int(3) NOT NULL,
   `judul` varchar(60) NOT NULL,
   `id_penerbit` int(2) NOT NULL,
   `singkatan` varchar(30) NOT NULL,
-  `sponsor` varchar(100) NOT NULL,
   `e_issn` varchar(15) NOT NULL,
   `p_issn` varchar(15) NOT NULL,
-  `english` varchar(10) NOT NULL,
+  `english` tinyint(1) NOT NULL,
   `thn_mulai` varchar(4) NOT NULL,
-  `no_terakhir` varchar(1) NOT NULL,
-  `terbit_terakhir` int(4) NOT NULL,
-  `frekuensi` int(2) NOT NULL,
-  `bln_terbit1` int(2) NOT NULL,
-  `bln_terbit2` int(2) NOT NULL,
-  `bln_terbit3` int(2) NOT NULL,
-  `bln_terbit4` int(2) NOT NULL,
-  `doi` varchar(10) NOT NULL,
-  `mpgundip` varchar(10) NOT NULL
+  `no_terakhir` int(11) NOT NULL,
+  `terbit_terakhir` varchar(4) NOT NULL,
+  `doi` varchar(10) DEFAULT NULL,
+  `mpgundip` tinyint(1) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `peringkat_sinta` varchar(2) NOT NULL,
+  `url_sinta` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `jurnal`
 --
 
-INSERT INTO `jurnal` (`id_jurnal`, `no_jurnal`, `id_pengelola`, `judul`, `id_penerbit`, `singkatan`, `sponsor`, `e_issn`, `p_issn`, `english`, `thn_mulai`, `no_terakhir`, `terbit_terakhir`, `frekuensi`, `bln_terbit1`, `bln_terbit2`, `bln_terbit3`, `bln_terbit4`, `doi`, `mpgundip`) VALUES
-(1, 85, 3, 'Jurnal Anestesiologi Indonesia', 2, 'Janesti', 'Perhimpunan Dokter Spesialis Anestesiologi dan Terapi Intensif (PERDATIN)', '2089-970X', '2337-5124', 'tidak', '2012', '1', 2016, 3, 3, 6, 11, 0, '10.14710', 'tidak'),
-(2, 4, 8, 'ILMU KELAUTAN: Indonesian Journal of Marine Sciences', 3, 'IJMS', 'Association of Indonesian Coastal Management Experts (HAPPI)', '2406-7598', '0853-7291', 'ya', '2012', '4', 2017, 4, 3, 6, 9, 12, '10.14710', 'ya'),
-(3, 80, 12, 'International Journal of Renewable Energy Development', 4, 'IJRED', '', '2252-4940', '', 'ya', '2012', '3', 2017, 3, 2, 7, 10, 0, '10.14710', 'ya');
+INSERT INTO `jurnal` (`id_jurnal`, `no_jurnal`, `id_pengelola`, `judul`, `id_penerbit`, `singkatan`, `e_issn`, `p_issn`, `english`, `thn_mulai`, `no_terakhir`, `terbit_terakhir`, `doi`, `mpgundip`, `url`, `peringkat_sinta`, `url_sinta`) VALUES
+(1, 85, 3, 'Jurnal Anestesiologi Indonesia', 2, 'Janesti', '2089-970X', '2337-5124', 0, '2012', 1, '2016', '10.14710', 0, '', '', ''),
+(2, 4, 8, 'ILMU KELAUTAN: Indonesian Journal of Marine Sciences', 3, 'IJMS', '2406-7598', '0853-7291', 0, '2012', 4, '2017', '10.14710', 0, '', '', ''),
+(3, 80, 12, 'International Journal of Renewable Energy Development', 4, 'IJRED', '2252-4940', '', 0, '2012', 3, '2017', '10.14710', 0, '', '', '');
 
 -- --------------------------------------------------------
 
@@ -308,7 +338,7 @@ CREATE TABLE `last_update` (
 --
 
 CREATE TABLE `lembaga` (
-  `id_lembaga` int(3) NOT NULL,
+  `id_lembaga` int(11) NOT NULL,
   `nama_lembaga` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -354,7 +384,7 @@ CREATE TABLE `pengindeks` (
   `nama` varchar(20) NOT NULL,
   `id_pengindeks` int(2) NOT NULL,
   `tingkatan` varchar(2) NOT NULL,
-  `grade` varchar(20) NOT NULL
+  `grade` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -362,11 +392,12 @@ CREATE TABLE `pengindeks` (
 --
 
 INSERT INTO `pengindeks` (`nama`, `id_pengindeks`, `tingkatan`, `grade`) VALUES
-('Scopus', 1, '', ''),
-('DOAJ', 2, '', ''),
-('ESCI', 3, '', ''),
-('ACI', 4, '', ''),
-('Google Scholar', 5, '', '');
+('Scopus', 1, 'S2', 'Internasional Sedang'),
+('DOAJ', 2, 'S2', 'Internasional Sedang'),
+('ESCI', 3, 'S3', 'Internasional rendah'),
+('ACI', 4, 'S4', 'Nasional Tinggi'),
+('Google Scholar', 14, 'S3', 'Internasional Rendah'),
+('aaa', 16, 'S3', 'Internasional Rendah');
 
 -- --------------------------------------------------------
 
@@ -375,18 +406,20 @@ INSERT INTO `pengindeks` (`nama`, `id_pengindeks`, `tingkatan`, `grade`) VALUES
 --
 
 CREATE TABLE `portal` (
+  `id_portal` int(2) NOT NULL,
   `nama_portal` varchar(10) NOT NULL,
-  `id_portal` int(2) NOT NULL
+  `base_url` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `portal`
 --
 
-INSERT INTO `portal` (`nama_portal`, `id_portal`) VALUES
-('ejournal1', 1),
-('ejournal2', 2),
-('ejournal3', 3);
+INSERT INTO `portal` (`id_portal`, `nama_portal`, `base_url`) VALUES
+(1, 'ejournal1', 'https://ejournal.undip.ac.id/index.php'),
+(2, 'ejournal2', 'https://ejournal2.undip.ac.id/index.php'),
+(3, 'ejournal3', 'https://ejournal3.undip.ac.id/index.php'),
+(4, 'ejournal4', 'https://ejournal4.undip.ac.id/index.php');
 
 -- --------------------------------------------------------
 
@@ -400,9 +433,31 @@ CREATE TABLE `sk` (
   `no_sk` int(20) NOT NULL,
   `tanggal_mulai` date NOT NULL,
   `tanggal_berakhir` date NOT NULL,
-  `tanggal_penetapan` date NOT NULL,
-  `peringkat` varchar(2) NOT NULL,
-  `url_sinta` int(80) NOT NULL
+  `tanggal_penetapan` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sk_jurnal`
+--
+
+CREATE TABLE `sk_jurnal` (
+  `id` int(11) NOT NULL,
+  `id_jurnal` int(11) NOT NULL,
+  `id_sk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sponsor`
+--
+
+CREATE TABLE `sponsor` (
+  `id` int(3) NOT NULL,
+  `id_jurnal` int(3) NOT NULL,
+  `nama_sponsor` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -414,6 +469,12 @@ CREATE TABLE `sk` (
 --
 ALTER TABLE `auth`
   ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indexes for table `bulan_penerbitan`
+--
+ALTER TABLE `bulan_penerbitan`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `data_pengelola`
@@ -488,6 +549,18 @@ ALTER TABLE `sk`
   ADD PRIMARY KEY (`id_sk`);
 
 --
+-- Indexes for table `sk_jurnal`
+--
+ALTER TABLE `sk_jurnal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sponsor`
+--
+ALTER TABLE `sponsor`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -495,19 +568,25 @@ ALTER TABLE `sk`
 -- AUTO_INCREMENT for table `auth`
 --
 ALTER TABLE `auth`
-  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `bulan_penerbitan`
+--
+ALTER TABLE `bulan_penerbitan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `data_pengelola`
 --
 ALTER TABLE `data_pengelola`
-  MODIFY `id_pengelola` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_pengelola` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `departemen`
 --
 ALTER TABLE `departemen`
-  MODIFY `id_dept` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
+  MODIFY `id_dept` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
 
 --
 -- AUTO_INCREMENT for table `fakultas`
@@ -519,7 +598,7 @@ ALTER TABLE `fakultas`
 -- AUTO_INCREMENT for table `jurnal`
 --
 ALTER TABLE `jurnal`
-  MODIFY `id_jurnal` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_jurnal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `jurnal_pengindeks`
@@ -543,7 +622,7 @@ ALTER TABLE `last_update`
 -- AUTO_INCREMENT for table `lembaga`
 --
 ALTER TABLE `lembaga`
-  MODIFY `id_lembaga` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+  MODIFY `id_lembaga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT for table `penerbit`
@@ -555,19 +634,31 @@ ALTER TABLE `penerbit`
 -- AUTO_INCREMENT for table `pengindeks`
 --
 ALTER TABLE `pengindeks`
-  MODIFY `id_pengindeks` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_pengindeks` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `portal`
 --
 ALTER TABLE `portal`
-  MODIFY `id_portal` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_portal` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `sk`
 --
 ALTER TABLE `sk`
   MODIFY `id_sk` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sk_jurnal`
+--
+ALTER TABLE `sk_jurnal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sponsor`
+--
+ALTER TABLE `sponsor`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
